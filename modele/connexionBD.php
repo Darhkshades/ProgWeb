06b970aea4f383ac54619b73435838ee68a2556f
Darhkshades <ofwgktagaf@gmail.com>
2017-03-21 23:58:54 +0100

    fichier permettant de se connecter à la base de donnée à inclure dans les autres fichiers

---
+++ modele/connexionBD.php
@@ -0,0 +1,14 @@
+<?php 
+
+	try
+		{
+			$Connect=new PDO ('mysql:host=localhost;dbname=goorgorlu','root','s');
+		}
+		catch(Exception $e)
+		{
+			die('Erreur'.$e->getMessage());
+		}
+		return $Connect;
+
+
+?>
